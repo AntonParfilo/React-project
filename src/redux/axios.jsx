5bebe6f172d0197db81c939e26328fc2e8f1f48c
Anton Parfilo <antoxa106@gmail.com>
2022-07-28 15:44:51 +0300

    Added try catch to axios requests

--- src/redux/axios.jsx
+++ src/redux/axios.jsx
@@ -1,52 +1,83 @@
 import axios from "axios";
 
 export const getProductsMain = async (limit) => {
-  const response = await axios.get(
-    "http://ragaban.zzz.com.ua/reactapi/db.php?getProductsMain=1&limit="+limit,
-    {
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    }
-  );
-  return response.data;
+  try{
+    const response = await axios.get(
+      "http://ragaban.zzz.com.ua/reactapi/db.php?getProductsMain=1&limit="+limit,
+      {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      }
+    );
+    return response.data;
+  }
+  catch(err){
+    alert(err);
+  }
 };
 export const getProductsCategory = async (id, sort) => {
-  const response = await axios.get(
-    "http://ragaban.zzz.com.ua/reactapi/db.php?getProductsCategory="+id+"&sort="+sort,
-    {
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    }
-  );
-  return response.data;
+  try{
+    const response = await axios.get(
+      "http://ragaban.zzz.com.ua/reactapi/db.php?getProductsCategory="+id+"&sort="+sort,
+      {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      }
+    );
+    return response.data;
+  }
+  catch(err){
+    alert(err);
+  }
 };
 export const getProduct = async (id) => {
-  const response = await axios.get(
-    "http://ragaban.zzz.com.ua/reactapi/db.php?getProduct="+id,
-    {
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    }
-  );
-  return response.data;
+  try{
+    const response = await axios.get(
+      "http://ragaban.zzz.com.ua/reactapi/db.php?getProduct="+id,
+      {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      }
+    );
+    return response.data;
+  }
+  catch(err){
+    alert(err)
+  }
+  
 };
 export const sendOrder = async (data) => {
-  const response = await axios.post(
-    "http://ragaban.zzz.com.ua/reactapi/index.php?",
-    {data: data, setOrder: 1}
-  );
-  return response.data;
+  try{
+    const response = await axios.post(
+      "http://ragaban.zzz.com.ua/reactapi/index.php?",
+      {data: data, setOrder: 1}
+    );
+    return response.data;
+  }
+  catch(err){
+    alert(err);
+  }
 };
 export const getFiltersCategory = async(name)=>{
-  const response = await axios.get(
-    "http://ragaban.zzz.com.ua/reactapi/db.php?getFiltersCategory="+name,
-    {
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    }
-  );
-  return response.data;
+  try{
+    const response = await axios.get(
+      "http://ragaban.zzz.com.ua/reactapi/db.php?getFiltersCategory="+name,
+      {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      }
+    );
+    return response.data;
+  }
+  catch(err){
+    alert(err);
+  }
 }
 export const getProductsCategoryFilters = async(data)=>{
-  const response = await axios.post(
-    "http://ragaban.zzz.com.ua/reactapi/db.php?",
-    {getProductsCategoryFilters: 1, filters: data.filters, category: data.category, sort: data.sort}
-  );
-  return response.data;
+  try{
+    const response = await axios.post(
+      "http://ragaban.zzz.com.ua/reactapi/db.php?",
+      {getProductsCategoryFilters: 1, filters: data.filters, category: data.category, sort: data.sort}
+    );
+    return response.data;
+  }
+  catch(err){
+    alert(err);
+  }
 }
