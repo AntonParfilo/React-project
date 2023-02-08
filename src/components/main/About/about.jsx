e483976c15833bda1568fd00f19c6f850d6185bc
John Doe <antoxa106@gmail.com>
2023-02-08 21:51:41 +0200

    comit

--- src/components/main/About/about.jsx
+++ src/components/main/About/about.jsx
@@ -7,8 +7,6 @@ const About = () => {
    
   return(
   <div>
-    <p>Это приложение создано в процессе изучения <b>React.</b></p>
-    <p> В процессе изучения использовал библиотеки:</p>
     <p><b>react</b></p>
     <p><b>redux</b></p>
     <p> <b>react-redux</b></p>
@@ -17,8 +15,9 @@ const About = () => {
     <p><b>Saga</b></p>
     <p><b>Axios</b></p>
     <p><b>Bootstrap</b></p>
-    <p>Так-же познакомился с Flux архитеркрурой, старался придерживаться <b>Flux</b> при разработке этого приложения.</p>
-    <p>На Backend стороне использовал <b>PHP+MySQL</b>.</p>
+    <p><b>FLUX</b></p>
+    <p><b>PHP</b></p>
+    <p><b>MySQL</b></p>
   </div>
   )
 };
